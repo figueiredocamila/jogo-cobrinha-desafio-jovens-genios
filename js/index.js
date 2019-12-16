53e26754f096cb7b8f5e1161b7ea3c6cd0e77401
figueiredocamila <camiladiasfigueiredo@gmail.com>
2019-12-15 23:32:51 -0200

    Add player 2

--- js/index.js
+++ js/index.js
@@ -13,9 +13,20 @@ var score = 0;
 
 var isPlaying = false;
 
+var numberOfPlayers = "";
+
+var _context;
+var _snakeX;
+var _snakeY;
+var _snake = [];
+var _tail;
+var _snakeXSpeed = 0;
+var _snakeYSpeed = 0;
+
 window.onload = function(){ 
     canvas = this.document.getElementById('arena');
     context = canvas.getContext('2d');
+    _context = canvas.getContext('2d')
     
     //comandos de controle
     document.addEventListener("keydown", keyPush);
@@ -24,15 +35,16 @@ window.onload = function(){
     snakeX = canvas.width/2 - box;
     snakeY = canvas.height/2 - box;
 
+
+    //iniciar a cobra no centro da tela
+    _snakeX = canvas.width/2 - box;
+    _snakeY = canvas.height/2 - box;
+
     //posição inicial da fruta
     this.fruitX = 5 * box;
     this.fruitY = 5 * box;
 
-    //funções que rodação em frames/segundo
-    this.setInterval(function(){
-        draw();
-        game();
-    }, 1000/7); 
+    menu();
 }
 
 function playMusic() {
@@ -51,7 +63,7 @@ function draw(){
     text('score: ' + score, '15px Gameplay', 5, 18, 'black');//setup score
 }
 
-function game(){
+function gamePlayer1(){
     //movimentação da cobra
     snakeX += snakeXSpeed;
     snakeY += snakeYSpeed;
@@ -103,6 +115,55 @@ function game(){
     }
 }
 
+function gamePlayer2() {
+    //movimentação da cobra
+    _snakeX += _snakeXSpeed;
+    _snakeY += _snakeYSpeed;
+
+    //BONUS-TRACK: cobra surge na direção oposta
+    if(_snakeX < 0){
+        _snakeX = canvas.width - box;
+    }
+    
+    if((_snakeX+box) > canvas.width){
+        _snakeX = 0;
+    }
+
+    if(_snakeY < 25){
+        _snakeY = canvas.height - box;
+    }
+
+    if((_snakeY+box) > canvas.height){
+        _snakeY = box;
+    }
+
+    _context.fillStyle = 'red';
+    for(var i = 0; i < _snake.length; i++){
+        _context.fillRect(_snake[i].x, _snake[i].y, box, box);
+
+        if(_snake[i].x == _snakeX && _snake[i].y == _snakeY){
+            _tail = 3;
+            _snakeXSpeed = 0;
+            _snakeYSpeed = 0;
+            score = 0;
+        }   
+    }
+
+    _snake.push({x: _snakeX, y: _snakeY});
+
+    while(_snake.length > _tail){
+        _snake.shift();
+    }
+
+    //Comendo a fruta
+    if(fruitX==_snakeX && fruitY == _snakeY){
+        _tail++;//BONUS-TRACK: A cobra cresce em 1 pixel a cada fruta comida.
+        score += 100;
+        fruitX = Math.round(Math.random() * (400 / box)) * box + box;//posição da nova fruta em X
+        fruitY = Math.round(Math.random() * (400 / box)) * box + box;//posição da nova fruta em Y
+    }
+}
+
 function rect(x, y, w, h, cor) {
 	context.fillStyle = cor;
 	context.fillRect(x, y, w, h);
@@ -115,7 +176,6 @@ function text(txt, fnt, x, y, cor) {
 }
 
 function keyPush(event){
-    playMusic();
 
     if (event.keyCode === 37) {
 		snakeXSpeed = -box;
@@ -136,4 +196,58 @@ function keyPush(event){
         snakeXSpeed = 0;
         snakeYSpeed = box;
     }
+    
+    if (event.keyCode === 65) {
+		_snakeXSpeed = -box;
+        _snakeYSpeed = 0;
+    } 
+
+    else if (event.keyCode === 87) {
+        _snakeXSpeed = 0;
+        _snakeYSpeed = -box;
+    } 
+
+    else if (event.keyCode === 68) {
+        _snakeXSpeed = box;
+        _snakeYSpeed = 0;
+    } 
+    
+    else if (event.keyCode === 83) {
+        _snakeXSpeed = 0;
+        _snakeYSpeed = box;
+	}
+}
+
+//Menu
+function menu(){
+    var numberOfPlayersButtons = document.querySelectorAll(".optionPlayer");
+    var startGameButton = document.getElementById("startGame");
+
+    numberOfPlayersButtons.forEach(function(el){
+        el.addEventListener("click", function(e){
+            e.preventDefault();
+            numberOfPlayers = this.dataset.players;
+            console.log(numberOfPlayers)
+            document.getElementById("menu").classList.add("hidden");
+            document.getElementById("manual").classList.remove("hidden");
+        })
+    })
+
+    startGameButton.addEventListener("click", function(e){
+        e.preventDefault();
+        document.getElementById("manual").classList.add("hidden");
+        startGame();
+    })
+}
+
+function startGame() {
+    playMusic();
+    //funções que rodação em frames/segundo
+    this.setInterval(function(){
+        draw();
+        gamePlayer1();
+        if(numberOfPlayers == "2") {
+            gamePlayer2();
+        }
+    }, 1000/6); 
 }
